67349d8c959a800ec3d24981ee496370d903f8d8
Samet Karaca <skaraca@onit.local>
2019-03-11 17:32:48 +0100

    Until Closures

--- ExperimentalJS.js
+++ ExperimentalJS.js
@@ -27,7 +27,7 @@ console.log(John.field) //We populate constructor variables from outside via pro
 //The newly created object inherits from the constructor's prototype property in Constructor function
 //Object.create makes complex inheritance structures simpler. It directly specifies which object should be a prototype.
 
-var personProto = {
+/* var personProto = {
         calculateAge: function(){
                 console.log(2019 - this.yearOfBirth)
         }
@@ -35,5 +35,102 @@ var personProto = {
 
 var John = Object.create(personProto)
 John.name = "Samet"
-John.yearOfBirth = "1990"
+John.yearOfBirth = "1990" */
+
+//TODO: PASSING A FUNCTION AS AN ARGUMENT (It is a good practice to write moduler functions than a huge and complciated one )
+
+/* var years = [1990, 1965, 1937, 2005, 1998]
+
+function arrayCalc(arr, fn) {
+        var arrRes = []
+        for (let i = 0; i < arr.length; i++) {
+                arrRes.push(fn(arr[i]))
+        }
+        return arrRes
+}
+
+function calculateAge(el){
+        return 2019 - el
+}
+
+function isFullAge(el){
+        return el >= 18
+}
+
+var ages = arrayCalc(years, calculateAge)
+var fullAges = arrayCalc(ages, isFullAge)
+
+console.log(ages)
+console.log(fullAges) */
+
+//calculateAge() functions is called meanwhile whereas calcualteAge alone is called later on (that is why it is calledback function). Isn't it awesome ! 
+
+//TODO: FUNCTIONS RETURNING FUNCTIONS
+/* 
+function interviewQuestion(job) {
+        if (job === "designer") {
+                return function (name) {
+                        console.log(name + " What is UX?")
+                }
+        } else if (job === "teacher") {
+                return function (name) {
+                        console.log(name + " What subject?")
+                }
+        } else {
+                return function (name) {
+                        console.log(name + " What do u do then chucky?")
+                }
+        }
+}
+
+var teacherQuestion = interviewQuestion("teacher")
+
+teacherQuestion("John")
+
+interviewQuestion("designer")("Mark") //We enter double parameters
+ */
+
+ //TODO:Immediately Invoked Function (IIF)
+
+//(function() {console.log("I am IIF")})()
+
+//TODO: Closures - An inner function has always access to the variables and parameters of its outer function, even after the aouter function has returned. 
+
+function retirement(retirementAge){
+        
+        return function(yearOfBirth){
+                
+                var sentence = " years left until your retirement"
+                var age = 2019 - yearOfBirth
+
+                console.log((retirementAge - age) + sentence)
+        }
+}
+
+var retirementUS = retirement(66)
+var retirementUK = retirement(64)
+var retirementDK = retirement(66)
+var retirementNO = retirement(68)
+var retirementTR = retirement(60)
+
+retirementUS(1990)
+retirement(80)(1990)
+
+//Example of Closure 
+
+function interviewQuestion(job){
+        return function(name){
+                if(job === "designer"){
+                        console.log(name + " , Can you explain what is UX?")
+                } else if (job === "musician") {
+                        console.log("Could u play Bach?")
+                } else {
+                        console.log("Hello " + name + ", What do you do?")
+                }
+        }
+}
+
+interviewQuestion("musician")("John")
+
+//TODO: Bind, Call and Apply
 
